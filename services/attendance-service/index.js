20649fedcf42a91fb332b1030b951f1968d4dda0
M. Khoirohman <2410511007@mahasiswa.upnvj.ac.id>
2026-04-29 16:17:19 +0700

    feat: gateway successfully connected to employee service

--- services/attendance-service/index.js
+++ services/attendance-service/index.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'db_attendance' // Pastikan nama DB ini sudah Anda buat di phpMyAdmin
+    host: 'localhost', user: 'root', password: '', database: 'db_attendance'
 });
 
-app.post('/attendance/checkin', (req, res) => {
-    const { employee_id, status } = req.body;
-    const query = 'INSERT INTO attendance (employee_id, status, timestamp) VALUES (?, ?, NOW())';
-    db.query(query, [employee_id, status], (err, result) => {
-        if (err) return res.status(500).json(err);
-        res.json({ message: "Absensi berhasil dicatat!", id: result.insertId });
-    });
+app.get('/attendance', (req, res) => {
+    res.json({ message: "Attendance Service connected to MySQL" });
 });
 
-app.listen(3002, () => console.log('Attendance Service menyala di port 3002'));
+app.listen(3002, () => console.log('Attendance Service (Port 3002) AKTIF'));
